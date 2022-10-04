cf9e76d6cf2afa5a30061e8aa71dc050de4c4480
Bolshakov Dmitry <vanove2013@yandex.ru>
2022-10-04 23:38:11 +0300

    init

---
+++ Factory/1-object.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const factory = (name, age, email) => {
+  return { name, age, email };
+};
+
+const factory2 = (name, age, email) => ({ name, age, email });
+
+const factory3 = (name, age, email) => {
+ name, age, email;
+};
+
+const factory4 = (name, age, email) => (name, age, email);
+
+
+console.log(factory('Alexandr', 30, 'suvorov@ya.ru'));
+console.log(factory2('Alexandr', 30, 'suvorov@ya.ru'));
+console.log(factory3('Alexandr', 30, 'suvorov@ya.ru'));
+console.log(factory4('Alexandr', 30, 'suvorov@ya.ru'));
+
---
+++ Factory/2-prototype.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const loggable = (fields) => {
+  function Loggable(data) {
+    this.values = data;
+  }
+
+  for (const key in fields) {
+    Object.defineProperty(Loggable.prototype, key, {
+      get() {
+        console.log('Reading prop: ' + key);
+        return this.values[key];
+      },
+      set(value) {
+        console.log('Writting prop: ', key, value);
+        const isValid = typeof value === fields[key].type && fields[key].validate(value);
+        if (!isValid) console.log('Validation failed: ' + key, value);
+        else this.values[key] = value;
+      }
+    });
+  }
+
+  Loggable.prototype.toString = function() {
+    let result = this.constructor.name + ': ';
+    for (const key in fields) {
+      result += this.values[key] + ' ';
+    }
+    return result;
+  }
+
+  return Loggable;
+};
+
+const Person = loggable({
+  name: { type: 'string', validate: (val) => val.length > 3 },
+  born: { type: 'number', validate: (val) => !(val % 1)}
+});
+
+const p1 = new Person({ name: 'Alexandr Suvorov', born: 1730 });
+console.log(p1.toString());
+p1.born = 1731;
+console.log(p1.born);
+p1.born = 100.5;
+p1.name = 'Victor Glushkov';
+console.log(p1.toString());
